3fafc2ca9a78947cbd471b7fa39cba312ff58ca8
G390tavo <jupiter390yt@gmail.com>
2025-06-16 18:08:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
   monedasAPI: "https://api.exchangerate.host/latest",
-  criptosAPI: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd",
+  criptosAPI: "https://api.coingecko.com/api/v3/coins/list",
   empresasEstaticas: [
     { nombre: "Apple", simbolo: "AAPL", sector: "Tecnología", pais: "EE.UU." },
-    { nombre: "Microsoft", simbolo: "MSFT", sector: "Tecnología", pais: "EE.UU." },
     { nombre: "Tesla", simbolo: "TSLA", sector: "Automotriz", pais: "EE.UU." },
-    { nombre: "Samsung", simbolo: "005930.KS", sector: "Electrónica", pais: "Corea del Sur" },
-    { nombre: "Toyota", simbolo: "7203.T", sector: "Automotriz", pais: "Japón" },
-    { nombre: "Nestlé", simbolo: "NESN.SW", sector: "Alimentos", pais: "Suiza" },
-    { nombre: "Alibaba", simbolo: "BABA", sector: "E-commerce", pais: "China" },
-    { nombre: "Amazon", simbolo: "AMZN", sector: "E-commerce", pais: "EE.UU." },
     { nombre: "Google", simbolo: "GOOGL", sector: "Tecnología", pais: "EE.UU." },
-    { nombre: "Meta", simbolo: "META", sector: "Tecnología", pais: "EE.UU." }
+    { nombre: "Amazon", simbolo: "AMZN", sector: "E-Commerce", pais: "EE.UU." },
+    { nombre: "Meta", simbolo: "META", sector: "Redes sociales", pais: "EE.UU." },
+    { nombre: "NVIDIA", simbolo: "NVDA", sector: "IA", pais: "EE.UU." },
+    { nombre: "Samsung", simbolo: "SSNLF", sector: "Tecnología", pais: "Corea del Sur" },
+    { nombre: "Sony", simbolo: "SONY", sector: "Electrónica", pais: "Japón" },
+    { nombre: "Toyota", simbolo: "TM", sector: "Automotriz", pais: "Japón" },
+    { nombre: "Alibaba", simbolo: "BABA", sector: "E-Commerce", pais: "China" }
   ]
 };
