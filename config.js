a3ffa402cc4f49a9c1f2f1ee4de1400cb2ed0f7c
G390tavo <jupiter390yt@gmail.com>
2025-06-16 22:21:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,4 @@
 const CONFIG = {
   monedasAPI: "https://api.exchangerate.host/latest",
-  criptosAPI: "https://api.coingecko.com/api/v3/coins/list",
-  empresasEstaticas: [
-    { nombre: "Apple", simbolo: "AAPL", sector: "Tecnología", pais: "EE.UU." },
-    { nombre: "Tesla", simbolo: "TSLA", sector: "Automotriz", pais: "EE.UU." },
-    { nombre: "Google", simbolo: "GOOGL", sector: "Tecnología", pais: "EE.UU." },
-    { nombre: "Amazon", simbolo: "AMZN", sector: "E-Commerce", pais: "EE.UU." },
-    { nombre: "Meta", simbolo: "META", sector: "Redes sociales", pais: "EE.UU." },
-    { nombre: "NVIDIA", simbolo: "NVDA", sector: "IA", pais: "EE.UU." },
-    { nombre: "Samsung", simbolo: "SSNLF", sector: "Tecnología", pais: "Corea del Sur" },
-    { nombre: "Sony", simbolo: "SONY", sector: "Electrónica", pais: "Japón" },
-    { nombre: "Toyota", simbolo: "TM", sector: "Automotriz", pais: "Japón" },
-    { nombre: "Alibaba", simbolo: "BABA", sector: "E-Commerce", pais: "China" }
-  ]
+  criptosAPI: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 };
