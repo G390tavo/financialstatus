f1350519b0980b8d31d0ecf11ea33533ce8264f2
G390tavo <jupiter390yt@gmail.com>
2025-06-18 10:34:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// Configuración general de la app
-const PROXY_URL = "http://localhost:3000/fetch?url=";
+const PROXY_URL = 'http://localhost:3000/fetch?url=';
 
-const FUENTES = {
-  monedas: "https://www.google.com/search?q=valor+de+monedas+internacionales",
-  criptos: "https://www.google.com/search?q=valor+de+criptomonedas",
-  empresas: "https://www.google.com/search?q=acciones+de+empresas",
-};
+const SOURCES = [
+  'https://www.google.com/search?q=',
+  'https://www.bing.com/search?q=',
+  'https://duckduckgo.com/?q='
+];
