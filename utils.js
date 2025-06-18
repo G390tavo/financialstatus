8ac1c5b5dbdebd2de280977f2ce8310b541b562c
G390tavo <jupiter390yt@gmail.com>
2025-06-17 22:51:31 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,13 +1,9 @@
-export function crearElemento(tipo, clase, contenido) {
+function crearElemento(tipo, contenido) {
   const el = document.createElement(tipo);
-  if (clase) el.className = clase;
-  if (contenido) el.textContent = contenido;
+  el.textContent = contenido;
   return el;
 }
 
-export function mostrarError(mensaje) {
-  const info = document.getElementById('info');
-  info.innerHTML = '';
-  const error = crearElemento('div', 'error', mensaje);
-  info.appendChild(error);
+function mostrarError(msg) {
+  alert("Error: " + msg);
 }
