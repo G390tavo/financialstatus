06884bf74870deda1c4f58c37aaf04265579226e
G390tavo <jupiter390yt@gmail.com>
2025-06-18 09:28:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const FUENTES = [
   "https://www.google.com/search?q=",
-  "https://www.bing.com/search?q=",
-  "https://duckduckgo.com/?q="
+  "https://www.bing.com/search?q="
 ];
 const PROXY_URL = "http://localhost:3000/fetch?url=";
+const ITEMS = {
+  monedas: ["dólar", "euro", "yen"],
+  criptos: ["bitcoin", "ethereum", "dogecoin"],
+  empresas: ["apple stock", "google stock", "amazon stock"]
+};
