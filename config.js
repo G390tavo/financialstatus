8f6f8f996465e3430cadeffc87e977c0a23a4fc4
G390tavo <jupiter390yt@gmail.com>
2025-06-28 16:45:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
-// Archivo: config.js
+// Configuración general del sistema
+const PROXY_URL = 'https://financial-proxy.onrender.com/?url=';
 
-const PROXY_URL = "https://financial-proxy.onrender.com/?url=";
+// Fuentes alternativas por tipo de dato
+const FUENTES_MONEDAS = [
+  'https://wise.com/gb/currency-converter/usd-to-pen-rate',
+  'https://www.x-rates.com/calculator/?from=USD&to=PEN&amount=1'
+];
 
-const FUENTES = {
-  monedas: [
-    "https://wise.com/gb/currency-converter/usd-to-pen-rate",
-    "https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=PEN"
-  ],
-  criptos: [
-    "https://coinmarketcap.com/currencies/bitcoin/",
-    "https://www.coindesk.com/price/bitcoin/",
-    "https://www.binance.com/en/price/bitcoin"
-  ],
-  empresas: [
-    "https://www.investing.com/equities/",
-    "https://www.marketwatch.com/tools/stockresearch/globalmarkets"
-  ]
-};
+const FUENTES_CRIPTOS = [
+  'https://coinmarketcap.com/',
+  'https://www.coingecko.com/'
+];
+
+const FUENTES_EMPRESAS = [
+  'https://www.investing.com/equities/',
+  'https://www.marketwatch.com/tools/stockresearch/globalmarkets'
+];
